eaea1ef381ea4527766b83a3d8f50df9ab39e57d
Nicolas Gasull <nicolas@gasull.net>
2016-05-18 19:21:10 +0200

    Test it and fix it

--- src/react-shallow-update.js
+++ src/react-shallow-update.js
@@ -1,9 +1,5 @@
 "use strict"
 
-function merge(objs) {
-  return extend({}, objs)
-}
-
 function extend(dest, objs) {
   if (!objs.length) {
     return dest
@@ -11,8 +7,8 @@ function extend(dest, objs) {
 
   var obj = objs[0]
   var rest = objs.slice(1)
-  obj.getOwnPropertyNames().map(function (prop) {
-    return o[prop] = obj[prop]
+  Object.getOwnPropertyNames(obj).map(function (prop) {
+    dest[prop] = obj[prop]
   })
   return extend(dest, rest)
 }
@@ -26,15 +22,15 @@ function shallowEqual(var1, var2) {
 }
 
 function objectShallowEqual(obj1, obj2) {
-  var props1 = obj1.getOwnPropertyNames()
-  var props2 = obj2.getOwnPropertyNames()
+  var props1 = Object.getOwnPropertyNames(obj1)
+  var props2 = Object.getOwnPropertyNames(obj2)
 
   if (props1.length !== props2.length) {
     return false
   }
 
   for (var i = 0; i < props1.length; i++) {
-    if (props1[i] !== props2[i]) {
+    if (obj1[props1[i]] !== obj2[props1[i]]) {
       return false
     }
   }
@@ -42,24 +38,80 @@ function objectShallowEqual(obj1, obj2) {
   return true
 }
 
-function makeReactExtensions() {
-
+function makeLifecycleExtensions() {
   return {
     $$reactShallowUpdated: true,
 
     shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState, nextContext) {
-      return ! (  objectShallowEqual(this.props, nextProps)
-               && objectShallowEqual(this.state, nextState)
-               && objectShallowEqual(this.context, nextContext)
-               )
+      var shouldUpdate = ! ( shallowEqual(this.props, nextProps)
+                          && shallowEqual(this.state, nextState)
+                          && shallowEqual(this.context, nextContext)
+                           )
+
+      if (process.env.NODE_ENV === 'dev') {
+        var g = global || window
+          , checkGlobal = typeof g.$$reactShallowPrevents === 'number'
+                       || typeof g.$$reactShallowPrevents === 'undefined'
+
+        if (!shouldUpdate && checkGlobal) {
+          g.$$reactShallowPrevents = (g.$$reactShallowPrevents || 0) + 1
+        }
+      }
+
+      return shouldUpdate
     }
   }
 }
 
-exports.monkeyPatchReact = function monkeyPatchReact(React) {
-  return extend([React.Component.prototype, makeReactExtensions()])
+function makeReactExtensions(React) {
+  var extensions = makeLifecycleExtensions()
+    , Component = wrapComponent(React, extensions)
+    , createClass = wrapCreateClass(React, extensions)
+
+  return {
+      $$reactShallowUpdated: true,
+      Component: Component,
+      createClass: createClass
+  }
 }
 
-exports.wrapReact = function wrapReact(React) {
-  return merge([React.Component.prototype, makeReactExtensions()])
+function checkShallowUpdated(React) {
+    return !!React.$$reactShallowUpdated
+}
+
+function wrapComponent(React, extensions) {
+
+  var Component = (function(ReactComponent) {
+    return function Component() {
+      return ReactComponent.apply(this, arguments)
+    }
+  })(React.Component)
+
+  extend(Component.prototype, [
+    React.Component.prototype,
+    extensions
+  ])
+
+  extend(Component, [{ prototype: Component.prototype }])
+  return Component
+}
+
+function wrapCreateClass(React, extensions) {
+  var reactCreateClass = React.createClass
+
+  return function createClass(userDef) {
+    var newArgs = Array.prototype.slice.call(arguments, 1)
+      , def = extend({}, [userDef, extensions])
+
+    newArgs.unshift(def)
+    return reactCreateClass.apply(this, newArgs)
+  }
+}
+
+exports.shallowUpdateMonkeyPatch = function monkeyPatchReact(React) {
+  return checkShallowUpdated(React) ? React : extend(React, [makeReactExtensions(React)])
+}
+
+exports.shallowUpdateWrap = function wrapReact(React) {
+  return checkShallowUpdated(React) ? React : extend({}, [React, makeReactExtensions(React)])
 }
